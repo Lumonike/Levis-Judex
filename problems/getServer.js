a487f31f00a3811ed494e782ca2cf83e06eeac87
VinkentLi <vinkentclicker27@gmail.com>
2025-03-27 18:34:26 -0400

    removed need for testcaseCount

--- problems/getServer.js
+++
@@ -1,4 +0,0 @@
-// Change this value to the domain or ip of the server
-// by default localhost:3000
-export const server = "https://judge.codejoint.org/";
-// alert("you haven't configured getServer.js in the problem folder yet!!")
