8715412e579b5884308055771fed8b6a93afdf03
Lumonike <robinwang121@gmail.com>
2025-03-29 19:44:56 -0400

    fixed W or L problem testcases

--- problems/3._W_or_L/submissions/code.py
+++ problems/3._W_or_L/submissions/code.py
@@ -1,3 +1,12 @@
-N = int(input())
-for i in range(N):
-    print("hi")
+s = input()
+w = 0
+l = 0
+for i in s:
+    if i == "L":
+        w += 1
+    else:
+        l += 1
+if w > l:
+    print("W")
+else:
+    print("L")
