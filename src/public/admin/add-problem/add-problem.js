960df8da60474ecd570a221700a7abd303449ed0
VinkentLi <vinkentclicker27@gmail.com>
2025-07-23 14:25:48 -0400

    license stuff

--- src/public/admin/add-problem/add-problem.js
+++ src/public/admin/add-problem/add-problem.js
@@ -1,3 +1,20 @@
+/* Levis Judex - Self-hosted platform for contests/problems
+ * Copyright (C) 2025 Vincent Li and Robin Wang
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 const $ = document.getElementById.bind(document);
 let inputTestcases = [];
 let outputTestcases = [];
@@ -62,13 +79,9 @@ function addSymbolButtons() {
             return button;
         }
 
-        // Add the ≤ and ≥ buttons
         formatGroup.appendChild(createSymbolButton('≤'));
         formatGroup.appendChild(createSymbolButton('≥'));
 
-        console.log(formatGroup);
-
-        // Append the button group to this toolbar
         toolbar.appendChild(formatGroup);
     });
 }
